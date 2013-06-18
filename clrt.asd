05640b12bf94c18fb527a2ce4d331e370f73748c
mike <mike@mike-1215N.(none)>
2013-06-18 18:22:54 -0400

    problem with scene.lisp

--- clrt.asd
+++ clrt.asd
@@ -11,7 +11,7 @@
 			   ((:file "linalg")
 				(:file "camera" :depends-on ("linalg"))
 				(:file "objects" :depends-on ("linalg" "camera"))
-				(:file "scene" :depends-on ("linalg" "camera" "objects"))
+				(:file "scene" :depends-on ("linalg" "camera" "objects"))))
 
 
 
--- scene.lisp
+++ scene.lisp
@@ -25,5 +25,42 @@
 (defun add-object (scene object)
   (push object (slot-value scene 'objects)))
 
+(defun render (scene width height filename)
+  (let* ((image (make-instance 'zpng:png
+							   :width width
+							   :height height))
+		 (image-data (zpng:data-array image))
+		 (delta (* 2 pi (/ (camera-fov (scene-camera scene)) 180.0)))
+		 (maxx (tan delta))
+		 (minx (- maxx))
+		 (maxy (* maxx (/ (coerce height 'real) (coerce width 'real))))
+		 (miny (-maxy))
+		 (stepx (/ (* 2 maxx) (coerce width 'real)))
+		 (stepy (/ (* 2 maxy) (coerce height 'real))))
+	(do ((y  0 (1+ y)))
+		 ((>= y height))
+	  (do* ((x 0 (1+ x))
+			(x-coord (+ minx (* x stepx)))
+			(y-coord (+ miny (* y stepy)))
+		   (ray-dir (make-vector 3 :data (make-array 3
+													 :element-type 'real
+													 :initial-contents (vector x-coord y-coord 1)))
+					(make-vector 3 :data (make-array 3
+													 :element-type 'real
+													 :initial-contents (vector x-coord y-coord 1)))))
+			((>= x maxx))
+		(setf (aref image-data y x 2) (255)))
+	  (zpng:write-png image filename :if-exists :supersede)))
 
-	
+
+
+
+
+
+
+
+
+
+
+
+
